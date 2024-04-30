169c155be20a2f3c3ef42220ebc095aaa0a3e041
Ottavio Monticelli <ottavio.monticelli@edu.unito.it>
2024-04-30 02:51:03 +0200

    major changes due to difference between RISC-V and x64. Updated versions of intercept.c and assembly files still need to be pushed. A lot of clean up to do

--- src/disasm_wrapper.c
+++ src/disasm_wrapper.c
@@ -90,7 +90,7 @@ intercept_disasm_init(const unsigned char *begin, const unsigned char *end)
 	 * Initialize the disassembler.
 	 * The handle here must be passed to capstone each time it is used.
 	 */
-	if (cs_open(CS_ARCH_X86, CS_MODE_64, &context->handle) != CS_ERR_OK)
+	if (cs_open(CS_ARCH_RISCV, CS_MODE_RISCV64, &context->handle) != CS_ERR_OK)
 		xabort("cs_open");
 
 	/*
@@ -145,9 +145,8 @@ check_op(struct intercept_disasm_result *result, cs_x86_op *op,
 	 */
 	const unsigned char *rip = code + result->length;
 
-	if (op->type == X86_OP_REG) {
-		if (op->reg == X86_REG_IP ||
-				op->reg == X86_REG_RIP) {
+	if (op->type == RISCV_OP_REG) {
+		if (op->reg == X86_REG_IP || op->reg == X86_REG_RIP) {
 			/*
 			 * Example: mov %rip, %rax
 			 */
@@ -207,23 +206,23 @@ struct intercept_disasm_result
 intercept_disasm_next_instruction(struct intercept_disasm_context *context,
 					const unsigned char *code)
 {
-	static const unsigned char endbr64[] = {0xf3, 0x0f, 0x1e, 0xfa};
+	//static const unsigned char endbr64[] = {0xf3, 0x0f, 0x1e, 0xfa}; // no endbr equivalent in RISC-V
 
 	struct intercept_disasm_result result = {.address = code, 0, };
 	const unsigned char *start = code;
 	size_t size = (size_t)(context->end - code + 1);
 	uint64_t address = (uint64_t)code;
 
-	if (size >= sizeof(endbr64) &&
-	    memcmp(code, endbr64, sizeof(endbr64)) == 0) {
-		result.is_set = true;
-		result.is_endbr = true;
-		result.length = 4;
-#ifndef NDEBUG
-		result.mnemonic = "endbr64";
-#endif
-		return result;
-	}
+// 	if (size >= sizeof(endbr64) &&
+// 	    memcmp(code, endbr64, sizeof(endbr64)) == 0) {
+// 		result.is_set = true;
+// 		result.is_endbr = true;
+// 		result.length = 4;
+// #ifndef NDEBUG
+// 		result.mnemonic = "endbr64";
+// #endif
+// 		return result;
+// 	}
 
 	if (!cs_disasm_iter(context->handle, &start, &size,
 	    &address, context->insn)) {
@@ -234,50 +233,67 @@ intercept_disasm_next_instruction(struct intercept_disasm_context *context,
 
 	assert(result.length != 0);
 
-	result.is_syscall = (context->insn->id == X86_INS_SYSCALL);
-	result.is_call = (context->insn->id == X86_INS_CALL);
-	result.is_ret = (context->insn->id == X86_INS_RET);
-	result.is_rel_jump = false;
-	result.is_indirect_jump = false;
+	result.is_syscall = (context->insn->id == RISCV_INS_ECALL);
+	// result.is_call = (context->insn->id == RISCV_INS_JAL ||
+	// 									context->insn->id == RISCV_INS_JALR);
+	// result.is_ret = (context->insn->id == RISCV_INS_JALR); // jalr è usata sia per ret che per le chiamate, per asserire o meno queste variabili servono verifiche diverse
+	// result.is_rel_jump = false;
+	// result.is_indirect_jump = false;
 #ifndef NDEBUG
 	result.mnemonic = context->insn->mnemonic;
 #endif
 
 	switch (context->insn->id) {
-		case X86_INS_JAE:
-		case X86_INS_JA:
-		case X86_INS_JBE:
-		case X86_INS_JB:
-		case X86_INS_JCXZ:
-		case X86_INS_JECXZ:
-		case X86_INS_JE:
-		case X86_INS_JGE:
-		case X86_INS_JG:
-		case X86_INS_JLE:
-		case X86_INS_JL:
-		case X86_INS_JMP:
-		case X86_INS_JNE:
-		case X86_INS_JNO:
-		case X86_INS_JNP:
-		case X86_INS_JNS:
-		case X86_INS_JO:
-		case X86_INS_JP:
-		case X86_INS_JRCXZ:
-		case X86_INS_JS:
-		case X86_INS_LOOP:
-		case X86_INS_CALL:
+		case RISCV_INS_BEQ: // these are jumps and their destination is calculated as PC + offset;
+		case RISCV_INS_BGE:
+		case RISCV_INS_BGEU:
+		case RISCV_INS_BLT:
+		case RISCV_INS_BLTU:
+		case RISCV_INS_BNE:
+		case RISCV_INS_JAL:
+			result.has_ip_relative_opr = true;
 			result.is_jump = true;
-			assert(context->insn->detail->x86.op_count == 1);
 			break;
-		case X86_INS_NOP:
-			result.is_nop = true;
+		case RISCV_INS_AUIPC:
+			result.has_ip_relative_opr = true;
+			break;
+		case RISCV_INS_JALR:
+			result.is_ret = true;
+			cs_riscv_op *op;
+			for (uint8_t op_i = 0; result.is_ret &&
+					op_i < context->insn->detail->riscv.op_count; ++op_i) {
+						op = context->insn->detail->riscv.operands + op_i;
+						switch (op->type) {
+							case RISCV_OP_REG:
+								if (op->reg != RISCV_REG_ZERO || op->reg != RISCV_REG_RA) {
+									result.is_ret = false;
+								}
+								break;
+							case RISCV_OP_IMM:
+								if (op->imm != 0) {
+									result.is_ret = false;
+								}
+								break;
+							case RISCV_OP_MEM:
+								if (op->mem.base != RISCV_REG_RA || op->mem.disp != 0) {
+									result.is_ret = false;
+								}
+								break;
+							default:
+								result.is_ret = false;
+								break;
+						}
+					}
+					if (!result.is_ret) {
+						result.is_jump = true;
+					}
 			break;
 		default:
 			result.is_jump = false;
+			result.has_ip_relative_opr = false;
 			break;
 	}
 
-	result.has_ip_relative_opr = false;
 
 	/*
 	 * Loop over all operands of the instruction currently being decoded.
@@ -293,26 +309,26 @@ intercept_disasm_next_instruction(struct intercept_disasm_context *context,
 	 * be relocated ( including relative jumps, which naturally also
 	 * rely on the RIP register ).
 	 */
-	for (uint8_t op_i = 0;
-	    op_i < context->insn->detail->x86.op_count; ++op_i)
-		check_op(&result, context->insn->detail->x86.operands + op_i,
-		    code);
+	// for (uint8_t op_i = 0; // needed? PC isn't usable as operand in RISC-V, so you know if instruction it's PC-relative from the instruction itself
+	//     op_i < context->insn->detail->x86.op_count; ++op_i)
+	// 	check_op(&result, context->insn->detail->x86.operands + op_i,
+	// 	    code);
 
-	result.is_lea_rip = (context->insn->id == X86_INS_LEA &&
-			result.has_ip_relative_opr);
-
-	if (result.is_lea_rip) {
-		/*
-		 * Extract the four bits from the encoding, which
-		 * specify the destination register.
-		 */
-
-		/* one bit from the REX prefix */
-		result.arg_register_bits = ((code[0] & 4) << 1);
-
-		/* three bits from the ModRM byte */
-		result.arg_register_bits |= ((code[2] >> 3) & 7);
-	}
+	// result.is_lea_rip = (context->insn->id == X86_INS_LEA && // no LEA instruction equivalent in RISC-V machine language
+	// 		result.has_ip_relative_opr);
+	//
+	// if (result.is_lea_rip) {
+	// 	/*
+	// 	 * Extract the four bits from the encoding, which
+	// 	 * specify the destination register.
+	// 	 */
+	//
+	// 	/* one bit from the REX prefix */
+	// 	result.arg_register_bits = ((code[0] & 4) << 1);
+	//
+	// 	/* three bits from the ModRM byte */
+	// 	result.arg_register_bits |= ((code[2] >> 3) & 7);
+	// }
 
 	result.is_set = true;
 
