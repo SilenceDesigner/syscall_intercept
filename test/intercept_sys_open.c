b8b3408d9fc3021d9a5135eb9f643c803f2236ab
Marco Edoardo Santimaria <msantimaria@sifive1.di.unito.it>
2024-10-04 10:39:41 +0000

    added simple openat interception test

---
+++ test/intercept_sys_open.c
@@ -0,0 +1,34 @@
+#include "libsyscall_intercept_hook_point.h"
+#include <stddef.h>
+#include <syscall.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+static int hook(long syscall_number,
+                long arg0, long arg1,
+                long arg2, long arg3,
+                long arg4, long arg5,
+                long *result)
+{
+    if (syscall_number == SYS_openat) {
+  	    write(1,"Proof of openat interception\n",29);
+    }
+    return 1;
+}
+
+static __attribute__((constructor)) void init(void)
+{
+    intercept_hook_point = hook;
+}
+
+int main() {
+    int fd = openat(AT_FDCWD,"non_existing_original_file.txt", O_RDONLY);
+    if (fd >= 0) {
+        write(1, "Success: File opened\n", 21);
+    } else {
+        write(1, "Failure: File not opened\n", 25);
+    }
+}
